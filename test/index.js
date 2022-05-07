a170bf2bf8603d54504411f4dd7876d6c5b2cfe5
Desco <63560727+Desco1@users.noreply.github.com>
2022-05-08 00:15:51 +0200

    cata 50+ fix, v1.781

--- test/index.js
+++ test/index.js
@@ -15,12 +15,12 @@ const parseData = data => {
     console.log("======================");
 };
 
-lily.getWeight("sirdesco", true).then(console.log);
+lily.getWeight("LilysCatthing", true).then(console.log);
 lily.getWeight("e710ff36-fe33-4c0e-8401-bda9d24fa121", true).then(parseData);
-lily.getWeight("SirDesco", true).then(parseData);
+lily.getWeight("LilysCatthing", true).then(parseData);
 lily.getWeightFromUUID("e710ff36fe334c0e8401bda9d24fa121", true).then(parseData);
-lily.getWeightFromUsername("SirDesco", true).then(parseData);
-lily.getWeightFromUsername("LappySheep", true).then(parseData);
+lily.getWeightFromUsername("LilysCatthing", true).then(parseData);
+lily.getWeightFromUsername("LunasCatgirl", true).then(parseData);
 lily.getProfileWeightFromUsername("Ascynx", "Mango", true).then(parseData);
 lily.getProfileWeightFromUUID("0ce87d5afa5f4619ae78872d9c5e07fe", "Mango", true).then(parseData);
 lily.getProfileWeight("Ascynx", "Mango", true).then(parseData);
