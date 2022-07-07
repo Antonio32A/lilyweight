2cce8706bb038ff3345589fb1647b5cbdc222d3d
Desco <63560727+Desco1@users.noreply.github.com>
2022-07-07 13:31:04 +0200

    skill overflow fix, 2.6.3

--- test/index.js
+++ test/index.js
@@ -15,15 +15,17 @@ const parseData = data => {
     console.log("======================");
 };
 
-lily.getWeight("LilysCatthing", true).then(console.log);
+// Commenting the name ones out so I don't have to bother to change these every time I want to change IGN.
+// They would work properly with the correct ign, tho.
+//lily.getWeight("LilysCatthing", true).then(console.log);
 lily.getWeight("e710ff36-fe33-4c0e-8401-bda9d24fa121", true).then(parseData);
-lily.getWeight("LilysCatthing", true).then(parseData);
+//lily.getWeight("LilysCatthing", true).then(parseData);
 lily.getWeightFromUUID("e710ff36fe334c0e8401bda9d24fa121", true).then(parseData);
-lily.getWeightFromUsername("LilysCatthing", true).then(parseData);
-lily.getWeightFromUsername("LunasCatgirl", true).then(parseData);
-lily.getProfileWeightFromUsername("Ascynx", "Mango", true).then(parseData);
+//lily.getWeightFromUsername("LilysCatthing", true).then(parseData);
+//lily.getWeightFromUsername("LunasCatgirl", true).then(parseData);
+//lily.getProfileWeightFromUsername("Ascynx", "Mango", true).then(parseData);
 lily.getProfileWeightFromUUID("0ce87d5afa5f4619ae78872d9c5e07fe", "Mango", true).then(parseData);
-lily.getProfileWeight("Ascynx", "Mango", true).then(parseData);
+//lily.getProfileWeight("Ascynx", "Mango", true).then(parseData);
 
 const raw = Lily.getWeightRaw(
     [60, 60, 60, 60, 60, 60, 60, 60],
